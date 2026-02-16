d170da249bfaf59cbae738e5b43e2d1696e9e0f4
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-02-16 18:55:34 +0400

    wip(app): implement Game struct and functions PrintBoard, PlaceToken, and InitBoard.

---
+++ app.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Game struct {
+	board [][]Token
+	turn  int
+}
+
+type Token string
+
+const PlayerOneToken Token = "1"
+const PlayerTwoToken Token = "2"
+
+func InitBoard(game *Game) {
+	game.board = make([][]Token, 6) // make columns
+	// make rows
+	for i := 0; i < 6; i++ {
+		game.board[i] = make([]Token, 0, 5)
+	}
+}
+
+func PlaceToken(game *Game, column int, token Token) error {
+	if column > 6 {
+		return fmt.Errorf("%v is out of bounds", column)
+	}
+	if column < 0 {
+		return fmt.Errorf("%v is out of bounds", column)
+	}
+	game.board[column] = append(game.board[column], token)
+	return nil
+}
+
+func (game Game) PrintBoard() {
+	// rows
+	for row := 7; row > -1; row-- {
+		for column := 0; column < 8; column++ {
+			if column > len(game.board)-1 || row > len(game.board[column])-1 {
+				fmt.Print(" ")
+				continue
+			}
+			fmt.Print(game.board[column][row])
+		}
+		fmt.Print("\n")
+	}
+	// visual numbers so players don't get confused
+	for i := 0; i < 7; i++ {
+		fmt.Print(i)
+	}
+	fmt.Print("\n")
+}
